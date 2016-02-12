8c479bec0d956216796f852c78a4a827a396fba7
Michael41000 <mrollber@purdue.edu>
2016-02-11 21:35:30 -0500

    Added Mod Status for questions

--- app/src/main/java/com/statfinder/statfinder/AddQuestionActivity.java
+++ app/src/main/java/com/statfinder/statfinder/AddQuestionActivity.java
@@ -1,29 +1,32 @@
 package com.statfinder.statfinder;
 
-import android.app.ActionBar;
-import android.app.Activity;
+import android.Manifest;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.net.Uri;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.WindowManager;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.Adapter;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
-
-import java.util.ArrayList;
+import android.widget.Toast;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
 
 public class AddQuestionActivity extends AppCompatActivity {
 
@@ -39,6 +42,8 @@ public class AddQuestionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_question);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        Firebase.setAndroidContext(this);
 
         final EditText question = (EditText) findViewById(R.id.questionText);
         final EditText firstAnswer = (EditText) findViewById(R.id.answer1);
@@ -49,9 +54,10 @@ public class AddQuestionActivity extends AppCompatActivity {
         final CheckBox otherCheckBox = (CheckBox) findViewById(R.id.otherCheckBox);
         final Button addAnswer = (Button) findViewById(R.id.addButton);
         final Button minusAnswer = (Button) findViewById(R.id.minusButton);
+        final Button submitButton = (Button) findViewById(R.id.submitButton);
         final View focusTheif = findViewById(R.id.focus_thief);
 
-        ((InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE)).toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
+        ((InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE)).toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
         question.requestFocus();
 
         question.setOnEditorActionListener(new EditText.OnEditorActionListener() {
@@ -83,7 +89,7 @@ public class AddQuestionActivity extends AppCompatActivity {
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
                 if (actionId == EditorInfo.IME_ACTION_DONE) {
                     focusTheif.requestFocus();
-                    InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
                     imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
 
                 }
@@ -95,7 +101,7 @@ public class AddQuestionActivity extends AppCompatActivity {
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
                 if (actionId == EditorInfo.IME_ACTION_DONE) {
                     focusTheif.requestFocus();
-                    InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
                     imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
 
                 }
@@ -107,7 +113,7 @@ public class AddQuestionActivity extends AppCompatActivity {
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
                 if (actionId == EditorInfo.IME_ACTION_DONE) {
                     focusTheif.requestFocus();
-                    InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
                     imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
 
                 }
@@ -119,7 +125,7 @@ public class AddQuestionActivity extends AppCompatActivity {
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
                 if (actionId == EditorInfo.IME_ACTION_DONE) {
                     focusTheif.requestFocus();
-                    InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
                     imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
 
                 }
@@ -131,16 +137,32 @@ public class AddQuestionActivity extends AppCompatActivity {
         addAnswer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                numAnswers++;
-                if (numAnswers == 3) {
+
+                if (numAnswers == 2) {
+                    if (secondAnswer.getText().length() == 0)
+                    {
+                        Toast.makeText(getApplicationContext(), "Please enter a second answer before adding third", Toast.LENGTH_LONG).show();
+                        return;
+                    }
                     minusAnswer.setVisibility(View.VISIBLE);
                     thirdAnswer.setVisibility(View.VISIBLE);
-                } else if (numAnswers == 4) {
+                } else if (numAnswers == 3) {
+                    if (thirdAnswer.getText().length() == 0)
+                    {
+                        Toast.makeText(getApplicationContext(), "Please enter a third answer before adding fourth", Toast.LENGTH_LONG).show();
+                        return;
+                    }
                     fourthAnswer.setVisibility(View.VISIBLE);
-                } else if (numAnswers == 5) {
+                } else if (numAnswers == 4) {
+                    if (fourthAnswer.getText().length() == 0)
+                    {
+                        Toast.makeText(getApplicationContext(), "Please enter a fourth answer before adding fifth", Toast.LENGTH_LONG).show();
+                        return;
+                    }
                     addAnswer.setVisibility(View.INVISIBLE);
                     fifthAnswer.setVisibility(View.VISIBLE);
                 }
+                numAnswers++;
             }
         });
 
@@ -162,6 +184,128 @@ public class AddQuestionActivity extends AppCompatActivity {
 
             }
         });
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if (question.getText().length() == 0)
+                {
+                    Toast.makeText(getApplicationContext(), "Please enter a question.", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if (firstAnswer.getText().length() == 0 && secondAnswer.getText().length() == 0)
+                {
+                    Toast.makeText(getApplicationContext(), "Please enter at least two answers.", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if (firstAnswer.getText().toString().equals(secondAnswer.getText().toString()))
+                {
+                    Toast.makeText(getApplicationContext(), "Please enter two different answers.", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                Location location = null;
+                double longitude;
+                double latitude;
+                if (ActivityCompat.checkSelfPermission(AddQuestionActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    location = getLastKnownLocation();
+                }
+                if (location == null) {
+                    Toast.makeText(getApplicationContext(), "Please enable location services so we could get your location", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                longitude = location.getLongitude();
+                latitude = location.getLatitude();
+                Geocoder geocoder = new Geocoder(AddQuestionActivity.this);
+                String city = null;
+                String state = null;
+                String country = null;
+                try {
+                    Address address = geocoder.getFromLocation(latitude, longitude, 1).get(0);
+                    city = address.getLocality();
+                    state = address.getAdminArea();
+                    country = address.getCountryName();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                final Firebase ref = new Firebase("https://statfinderproject.firebaseio.com/");
+                if (question.getText().toString().equals("Reset database") && firstAnswer.getText().toString().equals("Do") && secondAnswer.getText().toString().equals("It") && ((MyApplication) getApplication()).getUser().getModStatus())
+                {
+                    ref.child("Questions/").removeValue();
+                    ref.child("Questions/NumQuestions").setValue("0");
+                }
+                final HashMap<String, Object> idMap = new HashMap();
+                final HashMap<String, Object> questionInfo = new HashMap();
+                HashMap<String, String> answersMap = new HashMap();
+                answersMap.put(firstAnswer.getText().toString(), "0");
+                answersMap.put(secondAnswer.getText().toString(), "0");
+                if (thirdAnswer.getText().length() != 0 && thirdAnswer.getVisibility() == View.VISIBLE)
+                {
+                    answersMap.put(thirdAnswer.getText().toString(), "0");
+                }
+                if (fourthAnswer.getText().length() != 0 && fourthAnswer.getVisibility() == View.VISIBLE)
+                {
+                    answersMap.put(fourthAnswer.getText().toString(), "0");
+                }
+                if (fifthAnswer.getText().length() != 0 && fifthAnswer.getVisibility() == View.VISIBLE)
+                {
+                    answersMap.put(fourthAnswer.getText().toString(), "0");
+                }
+                if (otherCheckBox.isChecked())
+                {
+                    answersMap.put("Other", "0");
+                }
+                questionInfo.put("Name", question.getText().toString());
+                questionInfo.put("Answers", answersMap);
+                questionInfo.put("Flags", "0");
+                Long tsLong = System.currentTimeMillis()/1000;
+                String ts = tsLong.toString();
+                questionInfo.put("Time Created", ts);
+                questionInfo.put("Moderated", ((MyApplication) getApplication()).getUser().getModStatus());
+                //TODO: Add categories to the questions
+                // questionInfo.put("Categories", ...);
+                final String finalCity = city;
+                final String finalCountry = country;
+                final String finalState = state;
+                ref.child("Questions/NumQuestions").addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot snapshot) {
+                        int value = Integer.parseInt((String) snapshot.getValue(), 16);
+                        value++;
+                        String incHex = Integer.toHexString(value);
+                        ref.child("Questions/NumQuestions").setValue(incHex);
+                        ref.child("Questions/" + finalCity + "," + finalState + "," + finalCountry + "/" + (String) snapshot.getValue()).setValue(questionInfo);
+                        finish();
+                    }
+
+                    @Override
+                    public void onCancelled(FirebaseError firebaseError) {
+                    }
+                });
+            }
+        });
+    }
+
+    private Location getLastKnownLocation() {
+        LocationManager mLocationManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
+        List<String> providers = mLocationManager.getProviders(true);
+        Location bestLocation = null;
+        for (String provider : providers) {
+            Location l = null;
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                l = mLocationManager.getLastKnownLocation(provider);
+            }
+
+            if (l == null) {
+                continue;
+            }
+            if (bestLocation == null || l.getAccuracy() < bestLocation.getAccuracy()) {
+                // Found best last known location: %s", l);
+                bestLocation = l;
+            }
+        }
+        return bestLocation;
     }
 
 
