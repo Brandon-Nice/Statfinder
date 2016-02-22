b70d53031e75926b5eb33b39af0f3446d8d61635
Michael41000 <mrollber@purdue.edu>
2016-02-22 18:54:53 -0500

    Fixed bug with xml and

--- app/src/main/java/com/statfinder/statfinder/QuestionActivity.java
+++ app/src/main/java/com/statfinder/statfinder/QuestionActivity.java
@@ -21,8 +21,6 @@ public class QuestionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
