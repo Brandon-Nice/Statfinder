81b13fc05eb88110b73ffdef379efa7a9e784eb3
Michael41000 <mrollber@purdue.edu>
2016-02-22 18:52:34 -0500

    Updated xml for question activity

--- app/src/main/java/com/statfinder/statfinder/QuestionActivity.java
+++ app/src/main/java/com/statfinder/statfinder/QuestionActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
 
 public class QuestionActivity extends AppCompatActivity {
@@ -13,8 +14,8 @@ public class QuestionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
@@ -25,6 +26,11 @@ public class QuestionActivity extends AppCompatActivity {
 //            }
 //        });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
     }
 
 }
