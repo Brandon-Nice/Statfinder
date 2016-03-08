e149004015ba6c126042d0583aa6bf57ce816d2f
menane <menane@purdue.edu>
2016-03-08 11:47:35 -0500

    Added a QuestionFilter class with methods to start sorting out the poular and random questions. Will eventually put the history stuff in there as well

---
+++ app/src/main/java/com/statfinder/statfinder/QuestionFilterActivity.java
@@ -0,0 +1,42 @@
+package com.statfinder.statfinder;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class QuestionFilterActivity extends AppCompatActivity {
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_question_filter);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+//    }
+
+    //TODO: Create methods that filter popular and random questions
+    public void filterPopular(final String QuestionID){
+
+    }
+
+    public void filterRandom(final String QuestionID){
+
+    }
+
+    //TODO: Eventually put QuestionActivity stuff in here
+    public void filterHistory(final String QuestionID){
+
+    }
+}
