9d7b44abdbcdfd661765de100bfec08083aae6de
Brandon Nice <niceb@purdue.edu>
2016-03-23 20:17:49 -0400

    Added about and contact text, also changed color scheme

--- app/src/main/java/com/statfinder/statfinder/QuestionActivity.java
+++ app/src/main/java/com/statfinder/statfinder/QuestionActivity.java
@@ -5,6 +5,7 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.DialogPreference;
